6db5c567905de39f8568a7e46b2bb7ba22ea3bf5
Dennis Wilmsmann <denniswilmsmann@mailbox.org>
2024-04-14 10:21:00 +0200

    added some tests

--- maintenance_test.go
+++ maintenance_test.go
@@ -37,6 +37,31 @@ func TestGetRemoteIP(t *testing.T) {
 
 	if assert.NoError(t, getRemoteIP(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "127.0.0.1", rec.Body.String()) // Modify this as per your expectations
+		assert.Equal(t, "127.0.0.1", rec.Body.String())
+	}
+}
+
+func TestGetRemoteIPJSON(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/ip/json", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, getRemoteIPJSON(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, rec.Body.String(), "Timestamp")
+	}
+}
+
+func TestGetHeadersJSON(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/headers", nil)
+	req.Header.Set("X-Test-Header", "Test value")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, getHeadersJSON(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), "Test value")
 	}
 }
